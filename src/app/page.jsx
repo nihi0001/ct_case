30203deadc4c033205ed12d2378355b71c314f81
Lochmann20 <Linemoller1907@gmail.com>
2024-04-23 11:19:19 +0200

    page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,7 +13,7 @@ export default async function LandingPage({searchParams}){
   const response = await fetch(`https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`);
   const data = await response.json();
 
-  return (
+  return (  
     <>
       <h1>Product name</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo molestiae deserunt soluta, nam quam numquam odio enim? Vero dolorum veniam necessitatibus, eum beatae quas obcaecati mollitia nihil quia tempora facere?
