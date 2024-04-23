249b276e3b2a69a1be402205b8bde9943479a227
Lochmann20 <Linemoller1907@gmail.com>
2024-04-23 09:51:34 +0200

    page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,39 @@
 import Image from "next/image";
+export const dynamic = "force-dynamic";
 
-export default function Home() {
+export const metadata = {
+  title: "Fronpage",
+  description: "Description",
+};
+
+export default async function Home() {
+
+  const params = new URLSearchParams({
+    url: "https://www.charlietango.dk",
+  });
+
+  const response = await fetch(
+    `https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`,
+  );
+  const data = await response.json();
+
   return (
     <>
+    <h1>Product name</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo molestiae deserunt soluta, nam quam numquam odio enim? Vero dolorum veniam necessitatibus, eum beatae quas obcaecati mollitia nihil quia tempora facere?
+    </p>
+
+    <form>
+      <input 
+      type="url"
+      name="url"
+      id="url"
+      placeholder="https://example.com"
+      required
+      />
+    </form>
+
+    <button type="submit">Check it</button>
     </>
   );
 }
