27433e921fa28648a8f74de63ee3b9c200906054
Lochmann20 <Linemoller1907@gmail.com>
2024-04-25 11:58:29 +0200

    header

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+export default function Header() {
+    // const res = await fetch(`https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`);
+    // const menu = await res.json();
+
+  return (
+    <>
+    <nav className='fixed bottom-0 bg-yellow-100 w-full md:relative'>
+        <ul className='grid grid-cols-3 p-3 text-center items-center'>
+            <li>
+                <Link href={"/check"} prefetch={false}>Check Website</Link>
+            </li>
+            <li>
+                <Link className='inline-flex' href={"/home"} prefetch={false}>
+                <img src="/icon.svg" alt="logo" height={50} width={50}/>
+                </Link>
+            </li>
+            <li>
+                <Link href={"/regler"} prefetch={false}>Regler</Link>
+            </li>
+        </ul>
+    </nav>
+    </>
+  );
+}
+
