b13f2d91520b7e2d918d6740a4580319fee611fb
Frederikke <frny0001@stud.kea.dk>
2024-04-26 10:10:29 +0200

    javascript rules data

---
+++ src/app/rules/rules.js
@@ -0,0 +1,21 @@
+export const data = [
+    {
+      rulename: "image-alt",
+      description: "Ensures <img> elements have alternate text or a role of none or presentation"
+    },
+    {
+      rulename: "html-has-lang",
+      description: "Ensures every HTML document has a lang attribute"
+    },
+    {
+      rulename: "heading-order",
+      description: "Ensures the order of headings is semantically correct"
+    },
+    {
+      rulename: "inaplicacable",
+      description: "lorem ipsum4"
+    },
+  ];
+  
+   
+    
