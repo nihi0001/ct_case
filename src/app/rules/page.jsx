4b2cbda6b64ad52fda5979b69a3ed6931ab97ba4
Nina <nihi0001@kea.dk>
2024-04-26 14:13:02 +0200

    rules

--- src/app/rules/page.jsx
+++ src/app/rules/page.jsx
@@ -5,15 +5,17 @@ export default async function Page() {
      console.log(data);
   
     return (
-      <main>
-        <h1>Rules</h1>
-      <ul>
+      <main className="p-6 items-center max-w-screen-lg mx-auto mt-12">
+       <div>
+        <h1 className="text-4xl font-semibold mb-12">Rules</h1>
+      <ul className="space-y-7">
         {data.map(rule => (
-          <li key={rule.rulename}>{rule.rulename}</li>
+          <li key={rule.rulename}className="p-2 border-b text-lg" >{rule.rulename}</li>
           
         ))}
        
       </ul>
+      </div>
       </main>
     );
   } 
