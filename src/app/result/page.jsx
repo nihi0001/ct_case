13b06f8d91bc67e41ef892481ccc602460690f4e
Frederikke <frny0001@stud.kea.dk>
2024-04-30 10:38:25 +0200

    scoreresultani

--- src/app/result/page.jsx
+++ src/app/result/page.jsx
@@ -1,6 +1,6 @@
-
+import { score } from "@/lib/calculatorResult";
  import Image from "next/image";
-
+ import ResultScore from "@/app/components/ResultScore"
 
 // Revalidate route every 30 minutes
 //export const revalidate = 1800;
@@ -12,6 +12,14 @@ export default async function Page({ searchParams }) {
   );
   const data = await response.json();
 
+
+  const total = score(data);
+  const webScore = total[0];
+  const majorError = total[1];
+  const moderatError = total[2];
+  const minorError = total[3];
+
+
   return (
     <main>
       <h1>Report for {data.url.substring(0, 30)}...</h1>
@@ -19,9 +27,17 @@ export default async function Page({ searchParams }) {
       <Image
         alt={data.url}
         src={data.screenshot.url}
-        width={data.screenshot.width}
-        height={data.screenshot.height}
+        width={450}
+        height={500}
       />
+      <ResultScore data={webScore} />
+      <article>
+      <h2 className="text-lg">{majorError.length} Kritiske fejl</h2>
+
+      <h2 className="text-lg grow">{moderatError.length} Moderate fejl</h2>
+
+      <h2 className="text-lg">{minorError.length} Mindre fejl</h2>
+      </article>
     </main>
   );
 } 
