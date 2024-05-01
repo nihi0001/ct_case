5a23eb0fdfe580d14d53856d2d62b55e2408b345
Lochmann20 <Linemoller1907@gmail.com>
2024-05-01 12:52:44 +0200

    singlepageview

--- src/app/rules/[id]/page.jsx
+++ src/app/rules/[id]/page.jsx
@@ -1,5 +1,6 @@
 // rules single page
 import { data } from "../rules"
+import Link from 'next/link';
 
 export async function generateStaticParams() {
   return data.map((regl) => ({
@@ -15,22 +16,27 @@ export default async function Page({params}) {
     
 
     return (
-      <main>
-        <div className="">
+      <main className="">
+        <div className="p-8 md:p-16 md:px-44">
+        <ul className="bg-orange rounded-full w-16 p-3">
+        <Link href={"/rules"} prefetch={false}>
         <svg 
         width={40}
         height={40}
-        xmlns="http://www.w3.org/2000/svg" 
+        xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 448 512">
         <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/>
-        </svg>
+        </svg> 
+        </Link>
+        </ul>
         </div>
-        <div className="p-8 md:p-72 md:ps-96">
+
+        <div className="p-10 md:border-solid md:border-black md:border-2 md:p-20 md:w-7/12 md:m-auto">
         <h1 className="text-2xl font-semibold mb-4">{rule.rulename}</h1>
         <hr className="color:black md:w-full" />
-        <h2 className="mt-4 text-xl">{rule.impact}</h2> <br />
-        <p className="text-xl">{rule.longDescription}</p> <br />
-        <p className="text-xl">{rule.issueType}</p>
+        <h2 className="mt-4 text-xl"><span className="font-medium">Regler: <br /></span>{rule.impact}</h2> <br />
+        <p className="text-xl"><span className="font-medium">Beskrivelse: </span><br />{rule.longDescription}</p> <br />
+        <p className=" text-xl"><span className="font-medium">Effekt: </span><br />{rule.issueType}</p>
         </div>
       </main>
     );
