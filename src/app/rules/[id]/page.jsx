c13ec20d9a709c56da93b1025e1304ba52f37446
Lochmann20 <Linemoller1907@gmail.com>
2024-05-01 11:00:13 +0200

    singlepage

--- src/app/rules/[id]/page.jsx
+++ src/app/rules/[id]/page.jsx
@@ -16,7 +16,16 @@ export default async function Page({params}) {
 
     return (
       <main>
-        <div className="p-8 md:p-72">
+        <div className="">
+        <svg 
+        width={40}
+        height={40}
+        xmlns="http://www.w3.org/2000/svg" 
+        viewBox="0 0 448 512">
+        <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/>
+        </svg>
+        </div>
+        <div className="p-8 md:p-72 md:ps-96">
         <h1 className="text-2xl font-semibold mb-4">{rule.rulename}</h1>
         <hr className="color:black md:w-full" />
         <h2 className="mt-4 text-xl">{rule.impact}</h2> <br />
