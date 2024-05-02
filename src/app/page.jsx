72d4f12c0c3a6f33ccb91e088d386dd957b4d1a3
Lochmann20 <Linemoller1907@gmail.com>
2024-05-02 10:56:11 +0200

    fonts

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,10 +4,10 @@ export default function LandingPage() {
   return (  
     <div className="grid md:justify-center md:flex md:max-h-screen md:p-24 md:mt-10 md:gap-72">
     <div className="grid pt-20 p-10"> 
-    <h1 className="tracking-widest text-base md:text-2xl font-normal">Web tilgængeligheds validator</h1>
+    <h1 className="tracking-widest text-base text-grey-80 md:text-2xl font-normal">Web tilgængeligheds validator</h1>
     <img src="/diversa_frontpage.svg" alt="logo" height={300} width={300}	className="mb-12 mt-2"/>
 
-    <p className="text-xl mb-7 md:text-3xl md:leading-normal">Få dit website tjekket for tilgængelighed - 
+    <p className="text-xl text-grey-80 mb-7 md:text-3xl md:leading-normal">Få dit website tjekket for tilgængelighed - 
     <br /> 
     <span className="text-orange">gratis og øjeblikkeligt resultat!</span>
     </p>
@@ -29,7 +29,7 @@ export default function LandingPage() {
     </form>
     </div>
 
-    <div className="grid w-11/12 p-10 mb-16 md:items-center md:w-fit">
+    <div className="grid w-11/12 p-10 mb-16 md:m-0 md:items-center md:w-fit">
       <img src="/computer-check.png" alt="check" width={400} height={400} />
     </div>
     </div>
